aceece524c975928004a33ad95459a669587630d
MirkoGit1 <129416647+MirkoGit1@users.noreply.github.com>
2023-05-21 14:31:12 +0200

    Creazione dei metodi createNodeAdjacencyListFormXml, addNode e addEdge

--- src/it/tungsteno/fp/rovinePerdute/Main.java
+++ src/it/tungsteno/fp/rovinePerdute/Main.java
@@ -1,4 +1,11 @@
 package it.tungsteno.fp.rovinePerdute;
 
+import it.tungsteno.fp.rovinePerdute.parsing.Reader;
+
+import javax.xml.stream.XMLStreamException;
+
 public class Main {
+    public static void main(String[] args) throws XMLStreamException {
+        System.out.println(Reader.createNodesAdjacencyListFromXML(Reader.getInputFilePath()));
+    }
 }
--- src/it/tungsteno/fp/rovinePerdute/parsing/Reader.java
+++ src/it/tungsteno/fp/rovinePerdute/parsing/Reader.java
@@ -3,10 +3,15 @@ package it.tungsteno.fp.rovinePerdute.parsing;
 import it.ayman.fp.lib.Menu;
 import it.tungsteno.fp.rovinePerdute.pathfinding.Node;
 
+import javax.xml.stream.XMLEventReader;
 import javax.xml.stream.XMLInputFactory;
+import javax.xml.stream.XMLStreamException;
 import javax.xml.stream.XMLStreamReader;
+import javax.xml.stream.events.XMLEvent;
 import java.io.File;
 import java.io.FileInputStream;
+import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
 public class Reader {
@@ -40,16 +45,90 @@ public class Reader {
         return listOfFiles[choice].getPath();
     }
 
-    public static List<List<Node>> createNodeAdjacencyList(String xmlFilePath) {
+    /**
+     * Metodo per aggiungere un nodo all'arraylist di linkedList.
+     * Ad ogni nodo è assegnata una linkedList
+     * @param node il nodo da aggiungere
+     * @param list l'arraylist a cui aggiungere il nodo
+     */
+
+    private static void addNode(Node node, ArrayList<LinkedList<Node>> list) {
+        LinkedList<Node> currentNodeList = new LinkedList<>();
+        currentNodeList.add(node);
+        list.add(currentNodeList);
+    }
+
+
+    /**
+     * Metodo che aggiunge un nodo alla linkedList di un altro, ovvero ne crea il collegamento
+     * @param start nodo a cui aggiungere il nodo di destinazione
+     * @param destination nodo di destinazione
+     * @param list arraylist in cui sono presenti i nodi
+     */
+    private static void addEdge(int start, int destination, ArrayList<LinkedList<Node>> list) {
+        Node destinationNode = list.get(destination).get(0);
+        list.get(start).add(destinationNode);
+    }
+
+    /**
+     * Metodo che legge il file XML e crea l'adjacencyList con i nodi
+     * @param xmlFilePath
+     * @throws XMLStreamException
+     */
+
+    public static ArrayList<LinkedList<Node>> createNodesAdjacencyListFromXML (String xmlFilePath) throws XMLStreamException {
+        XMLInputFactory xmlInputFactory = null;
+        XMLStreamReader NodeXmlReader = null;
+
         try {
-            XMLInputFactory xmlInputFactory = XMLInputFactory.newInstance();
-            XMLStreamReader NodeXmlReader =
-                    xmlInputFactory.createXMLStreamReader(xmlFilePath, new FileInputStream(xmlFilePath));
+            xmlInputFactory = XMLInputFactory.newInstance();
+            NodeXmlReader = xmlInputFactory.createXMLStreamReader(xmlFilePath, new FileInputStream(xmlFilePath));
         } catch (Exception e) {
             System.out.println(READER_ERROR);
             System.out.println(e.getMessage());
         }
 
-        return null;
+        ArrayList<LinkedList<Node>> nodeList = new ArrayList<>();
+        boolean running = true;
+
+        while (running){
+            switch(NodeXmlReader.getEventType()) {
+                case (XMLEvent.START_ELEMENT) -> {
+                    String tag = NodeXmlReader.getLocalName();
+                    NodeXmlReader.next();
+                    int id = 0;
+                    switch (tag) {
+                        case "city":
+                            id = Integer.parseInt(NodeXmlReader.getAttributeValue(0));
+                            String name = NodeXmlReader.getAttributeLocalName(1);
+                            int xCoordinate = Integer.parseInt(NodeXmlReader.getAttributeValue(2));
+                            int yCoordinate = Integer.parseInt(NodeXmlReader.getAttributeValue(3));
+                            int height = Integer.parseInt(NodeXmlReader.getAttributeValue(4));
+                            Node node = new Node(id, name, xCoordinate, yCoordinate, height);
+                            addNode(node, nodeList);
+
+                        case "link":
+                            int destination = Integer.parseInt(NodeXmlReader.getText());
+                            addEdge(id, destination, nodeList);
+                    }
+
+                }
+                //Se si tratta di un tag di chiusura
+                case (XMLEvent.END_ELEMENT) -> {
+                    //Se è il closing-tag del comune allora termina di controllare per lo specifico comune
+                    if (NodeXmlReader.getLocalName().equals("map"))
+                        running = false;
+
+                    //In ogni caso se è un closing-tag andare al successivo
+                    NodeXmlReader.next();
+                }
+            }
+            //Questa condizione serve in modo che non dia un errore perché ci si trova alla fine del file
+            if (NodeXmlReader.hasNext())
+                NodeXmlReader.next();
+        }
+
+        return nodeList;
     }
+
 }
